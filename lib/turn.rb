472333fddd15b1cf5d46ef89cbaf487ad3ee2847
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-12 01:08:11 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,8 @@
+def display_board(board)
+  row = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts row
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts row
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
