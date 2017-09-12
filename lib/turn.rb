bad5e0771d93856fa348dec4b7e7aaf17815b3af
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-12 01:13:48 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def  input_to_index(input)
   index = input.to_i - 1
 end
 
-def valid_move?(index)
+def valid_move?(board, index)
   if index < 8
     if board[index] == "" || board[index] == " " || board[index] == nil
       return true
